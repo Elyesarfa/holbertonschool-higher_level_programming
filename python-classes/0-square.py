5cd37ee9e924936e5b15ff25c5920b5c507add67
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-01 20:06:06 +0100

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Creating a square class """
+"""Define an empty class Square"""
 
 
 class Square:
-    """ Creating a square class """
+    """Empty class Square"""
     pass
