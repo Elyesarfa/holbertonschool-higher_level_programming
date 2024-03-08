71dda963aa3e0f91cf00bd56a4aed15e2b583045
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-08 18:16:51 +0100

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,8 +11,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """ retrieves a dictionary representation """
-        if (type(attrs)) == list and all(type(i) == str for i in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        else:
-            return self.__dict__
+        if (type(attrs) is list and
+                all(type(i) is str for i in attrs)):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
