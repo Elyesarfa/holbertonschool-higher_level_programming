9d45dd9090a6ca84dd4b2cf1a574a4f94a826f91
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-27 00:36:45 +0100

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa."""
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    # Check if all required arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    try:
-        # Connect to MySQL database
-        db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306,
-        )
+    # Create cursor
+    cursor = db.cursor()
 
-        # Create a cursor object
-        cur = db.cursor()
+    # Execute the query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Execute SQL query
-        cur.execute("SELECT * FROM states ORDER BY id;")
+    # Fetch all the rows
+    rows = cursor.fetchall()
 
-        # Fetch all rows
-        rows = cur.fetchall()
+    # Print the result
+    for row in rows:
+        print(row)
 
-        # Print fetched rows
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close cursor and database connection
-        if 'cur' in locals() and cur:
-            cur.close()
-        if 'db' in locals() and db:
-            db.close()
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
